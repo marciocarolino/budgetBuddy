37f765134a8dce6a1fc25cdb4d0de8b8e748603b
Marcio Carolino <marciocarolino@gmail.com>
2023-09-08 20:39:00 -0300

    feat: starting the project.

---
+++ src/test/java/br/com/BudgetBuddy/BudgetBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.BudgetBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
