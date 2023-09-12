3a48cba4834a99fa6d276d38b12abd402d5c51c6
Marcio Carolino <marciocarolino@gmail.com>
2023-09-11 21:14:19 -0300

    feat: verify email update

--- src/main/java/br/com/BudgetBuddy/controllers/UserController.java
+++ src/main/java/br/com/BudgetBuddy/controllers/UserController.java
@@ -3,6 +3,7 @@ package br.com.BudgetBuddy.controllers;
 import br.com.BudgetBuddy.domain.user.RequestUser;
 import br.com.BudgetBuddy.domain.user.User;
 import br.com.BudgetBuddy.repository.UserRepository;
+import br.com.BudgetBuddy.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,62 +17,43 @@ import java.util.Optional;
 @RequestMapping("/user")
 public class UserController {
 
+
     @Autowired
-    private UserRepository userRepository;
+    UserService userService;
+
 
     @GetMapping
     public ResponseEntity<List<User>> getAllActiveUsers() {
-        List<User> activeUsers = userRepository.findByActivedTrue();
-        return ResponseEntity.ok(activeUsers);
+        return userService.getAllUsers();
 
     }
 
     @PostMapping()
     public ResponseEntity createUser(@RequestBody @Valid RequestUser data) {
+        return userService.createUser(data);
 
-        if (userRepository.existsByEmail(data.email())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Email já cadastrado");
-        }
-
-        User user = new User(data);
-        user.setActived(true);
-        userRepository.save(user);
-        return ResponseEntity.ok(user);
     }
 
     @PutMapping()
     public ResponseEntity updateUser(@RequestBody @Valid RequestUser data) {
-
-        Optional<User> optionalUser = userRepository.findById(data.id());
-
-        if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
-            user.setName(data.name());
-            user.setEmail(data.email());
-            user.setPassword(data.password());
-            user.setActived(data.actived());
-            return ResponseEntity.ok(user);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return userService.updateUser(data);
 
     }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity deleteUser(@PathVariable int id) {
-
-        Optional<User> optionalUser = userRepository.findById(id);
-
-        if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
-            user.setActived(false);
-            userRepository.save(user);
-            return ResponseEntity.ok(user);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-
-    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity deleteUser(@PathVariable int id) {
+//
+//        Optional<User> optionalUser = userRepository.findById(id);
+//
+//        if (optionalUser.isPresent()) {
+//            User user = optionalUser.get();
+//            user.setActived(false);
+//            userRepository.save(user);
+//            return ResponseEntity.ok(user);
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//    }
 
 }
---
+++ src/main/java/br/com/BudgetBuddy/service/UserService.java
@@ -0,0 +1,70 @@
+package br.com.BudgetBuddy.service;
+
+
+import br.com.BudgetBuddy.domain.user.RequestUser;
+import br.com.BudgetBuddy.domain.user.User;
+import br.com.BudgetBuddy.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> activeUsers = userRepository.findByActivedTrue();
+        return ResponseEntity.ok(activeUsers);
+    }
+
+    public ResponseEntity createUser(@RequestBody @Valid RequestUser data) {
+        if (userRepository.existsByEmail(data.email())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("E-mail já cadastrado!");
+        }
+
+        User user = new User(data);
+        user.setActived(true);
+        userRepository.save(user);
+        return ResponseEntity.ok(user);
+    }
+
+    public ResponseEntity updateUser(@RequestBody @Valid RequestUser data) {
+        Optional<User> optionalUser = userRepository.findById(data.id());
+
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+
+            if (!user.getEmail().equals(data.email())) {
+                // Verifica se o novo email já existe em outro usuário
+                if (userRepository.existsByEmail(data.email())) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                            .body("E-mail já cadastrado!");
+                }
+            }
+            user.setName(data.name());
+            user.setEmail(data.email());
+            user.setPassword(data.password());
+            user.setActived(data.actived());
+
+            userRepository.save(user);
+
+
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+}
