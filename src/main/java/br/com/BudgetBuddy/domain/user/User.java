817b206d9ff1e0fd4096d85eaddb619b4f0d2cca
Marcio Carolino <marciocarolino@gmail.com>
2023-10-22 23:23:30 -0300

    feat: config role user

--- src/main/java/br/com/BudgetBuddy/domain/user/User.java
+++ src/main/java/br/com/BudgetBuddy/domain/user/User.java
@@ -4,6 +4,12 @@ package br.com.BudgetBuddy.domain.user;
 import br.com.BudgetBuddy.dto.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "user")
 @Table(name = "user", schema = "public")
@@ -12,7 +18,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -30,11 +36,46 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "role")
+    private UserRole role;
+
 
     public User(UserDTO userDTO) {
         this.name = userDTO.name();
         this.email = userDTO.email();
         this.password = userDTO.password();
         this.actived = userDTO.actived();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
